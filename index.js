e1783ea9212400bafd3003d6a9c3c2959dde04cd
NADA MILONE <nadamilone@gmail.com>
2022-03-02 13:33:38 -0500

    commit tree 0d82fd89373b093a09597dacf6ef19b6a3b6c53f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+//  1) no longer has DOM node 'main#main'
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Nada is the champion!"
+newHeader.className = "victory";
+//  2) has a 'newHeader' variable that points to node 'h1#victory'
+//  3) has a 'newHeader' variable that points to node 'h1#victory'
+//  4) has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+
